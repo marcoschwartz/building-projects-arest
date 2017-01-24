bd03527ab657ea47f7c20528692750d1dbdf6083
Marco Schwartz <marcolivier.schwartz@gmail.com>
2017-01-24 09:26:03 +0100

    added robot project

---
+++ 07/esp8266_robot/interface/script.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+
+    // Device
+    var address = "192.168.0.115";
+    var device = new Device(address);
+
+    // Buttons
+    $('#forward').mousedown(function() {
+      console.log('Forward');
+      device.callFunction("forward");
+    });
+    $('#forward').mouseup(function() {
+      device.callFunction("stop");
+    });
+
+    $('#right').mousedown(function() {
+      device.callFunction("right");
+    });
+    $('#right').mouseup(function() {
+      device.callFunction("stop");
+    });
+
+    $('#left').mousedown(function() {
+      device.callFunction("left");
+    });
+    $('#left').mouseup(function() {
+      device.callFunction("stop");
+    });
+
+    $('#backward').mousedown(function() {
+      device.callFunction("backward");
+    });
+    $('#backward').mouseup(function() {
+      device.callFunction("stop");
+    });
+
+});
